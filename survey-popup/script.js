21a11b950dccd7bbebbb07c586ebdc53e1fe7340
Mohammed-Yousuf Hamid <mohammedyousufhamid@gmail.com>
2021-06-08 17:59:57 +0100

    <feat> Added survey popup component

---
+++ survey-popup/script.js
@@ -0,0 +1,16 @@
+const popup = document.querySelector(".popup__card");
+const popupBtn = document.querySelector("#popupOpen");
+const crossBtn = document.querySelector(".popup__cross");
+
+crossBtn.addEventListener("click", hidePopup);
+popupBtn.addEventListener("click", showPopup);
+
+function hidePopup() {
+  popup.style.display = 'none';
+  popupBtn.style.display = 'block';
+}
+
+function showPopup() {
+  popup.style.display = 'block';
+  popupBtn.style.display = 'none';
+}
